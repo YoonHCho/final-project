d008c148ec4706522029fe387ae5db38028581f9
Yoon Hwan Cho <yhcho0714@gmail.com>
2022-09-16 04:40:04 +0000

    completed feature-2 user can create log for a place

--- database/data.sql
+++ database/data.sql
@@ -1,4 +1,2 @@
 INSERT INTO "logs" ("log", "location", "latitude", "longitude")
 VALUES             ('testing log muhaha','LearningFuze', 33.6348748, -117.7404808);
--- INSERT INTO "logs" ("log", "location", "latitude", "longitude", "createAt")
--- VALUES             ('testing log muhaha','LearningFuze', 33.6348748, -117.7404808, now());
